6425625a6cefe082971aeb3662cea18e6da08312
Вьюгина Анна Сергеевна \ Anna Viugina <ViuginaAS@suek.ru>
2025-02-14 19:12:38 +0300

    Обновлены хэширование и добавление элемента в хэш-таблицу

--- HomeWork_HashTable/obj/Debug/net9.0/HomeWork_HashTable.AssemblyInfo.cs
+++ HomeWork_HashTable/obj/Debug/net9.0/HomeWork_HashTable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWork_HashTable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4f7b3337e79a08bbb01a98ab0dcfd7b6441cf4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7ee03ee7da00a724616353a44cc8250f1c39b78")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWork_HashTable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWork_HashTable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
